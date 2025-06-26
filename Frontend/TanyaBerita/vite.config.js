b479ac86d9431213b5fc4033b546c7d4eb67f59b
risya22008 <risyaannisachairyah1411@gmail.com>
2025-06-26 09:24:27 +0700

    feat: dockerize backend and frontend, add deploy workflow

--- Frontend/TanyaBerita/vite.config.js
+++ Frontend/TanyaBerita/vite.config.js
@@ -1,16 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd()) // load .env file
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BE_URL, // pastikan nama variabel diawali VITE_
+          changeOrigin: true,
+          secure: false,
+        },
       },
+      host: true, // This is crucial for Docker
+      port: 5173, // Make sure this matches your desired port
+      // If you're encountering issues with hot module reloading in Docker,
+      // you might also need:
+      // watch: {
+      //   usePolling: true,
+      // },
+      strictPort: true, // Optional: Fails if port is already in use
+      allowedHosts: true,
     },
-  },
+  }
+
 })
